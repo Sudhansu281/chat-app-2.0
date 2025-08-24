52daee95583e863d972ad7c431eb417da9d99500
Sudhansu281 <159606194+Sudhansu281@users.noreply.github.com>
2025-08-24 14:37:35 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json({
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-2-0-1.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
