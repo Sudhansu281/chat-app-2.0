aa285544e70c536b0c21c4ae19a3b0872cec1b7c
Sudhansu281 <159606194+Sudhansu281@users.noreply.github.com>
2025-08-24 14:50:34 +0530

    Update index.js

--- Frontend/src/apiCalls/index.js
+++ Frontend/src/apiCalls/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const url = "https://chat-app-2-0-lzju.onrender.com";
+export const url = "https://chat-app-2-0-3.onrender.com";
 export const axiosInstance = axios.create({
      headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`
